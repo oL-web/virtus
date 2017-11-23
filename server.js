6c8a85df3d7eee2fac1a0f542716a55c4ad4e96a
Michał <ol_web@outlook.com>
2017-11-23 19:32:29 +0100

    deploy

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname));
+
+/* app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+}); */
+
+io.on('connection', function(socket){
+  socket.on('message sent', function(msg){
+    io.emit('message received', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
